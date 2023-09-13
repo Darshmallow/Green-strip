f4d4ad124c43c07563e620c423ae0acdec79f210
Darshmallow <98087037+Darshmallow@users.noreply.github.com>
2023-09-13 13:22:32 -0700

    Add files via upload

---
+++ Schedule.java
@@ -0,0 +1,20 @@
+public class Schedule {
+    public static void main(String[] args) {
+        String border = "+-------------------------------+";
+        System.out.println(border);
+        String c1 = "Computer";
+        String t1 = "Mr. Zaremba";
+        System.out.println("|" + 1 + "|\t" + c1 + "\t|" +t1 + "\t|");
+        String c2 = "English";
+        String t2 = "Ms. Macphee";
+        System.out.println("|" + 2 + "|\t" + c2 + " \t|" +t2 + "\t|");
+        String c3 = "PHE";
+        String t3 = "Mr. Hodgson";
+        System.out.println("|" + 3 + "|\t" + c3 + "     \t|" + t3 + "\t|");
+        String c4 = "Science";
+        String t4 = "Ms. Kim";
+        System.out.println("|" + 4 + "|\t" + c4 + " \t|" +t4 + "    \t|");
+        System.out.println(border);
+
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int room = 113;
+        double e = 2.71828;
+        String learn = "Computer Science";
+        System.out.println("This is room # " + room);
+        System.out.println("e is roughly " + e);
+        System.out.println("I'm learning " + learn);
+    }
+}
